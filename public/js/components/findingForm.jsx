a0f0a077dcc354ed3133e3fe7a26bd5e9d78edd2
max_buranbaev <max@izst.ru>
2016-09-18 14:38:14 +0200

    removing bundle from git

--- public/js/components/findingForm.jsx
+++ public/js/components/findingForm.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 class FindingForm extends React.Component {
-    render() {
-      return (
-        <form className="navbar-form navbar-left" role="search">
-          <div className="form-group">
-            <input type="text" className="form-control" placeholder="Поиск" />
-          </div>
-        </form>
-      )
-    }
+  render() {
+    return (
+      <form className="navbar-form navbar-left" role="search">
+        <div className="form-group">
+          <input type="text" className="form-control" placeholder="Поиск" />
+        </div>
+      </form>
+    )
+  }
 }
 
 export default FindingForm;
