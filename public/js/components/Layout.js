a8719f74fe20f4754f0412cb8026e3d8571b639c
max_buranbaev <max@izst.ru>
2016-10-01 20:06:31 +0200

    creating layout

---
+++ public/js/components/Layout.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import TopPanel from './navbar/topPanel.jsx'
+import store from '../store.js'
+
+import { connect } from "react-redux"
+import { fetchGoods } from '../actions/goods'
+
+var Layout = React.createClass({
+
+    componentDidMount: function() {
+        store.dispatch(fetchGoods());
+    },
+
+    render: function() {
+        return (
+            <div>
+                <TopPanel />
+                <div className="container-fluid">
+                    { this.props.children }
+                </div>
+
+            </div>
+        )
+    }
+})
+
+export default Layout;
