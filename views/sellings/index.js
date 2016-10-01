99201ac766175200683a9cd055c585f30ede6c23
max_buranbaev <max@izst.ru>
2016-10-01 20:12:54 +0200

    new method for deleting selling

--- views/sellings/index.js
+++ views/sellings/index.js
@@ -1,18 +1,36 @@
 'use strict'
 
 exports.addSelling = function(req, res, next) {
-	var db = req.app.db;
-	var Selling = db.model('Selling');
-	console.log(req.body);
-	var newSelling = new Selling();
-	newSelling.good = req.body.id;
-	newSelling.phone = req.body.phone;
-	newSelling.source = req.body.source;
-	newSelling.date = Date.now();
+    var db = req.app.db;
+    var Selling = db.model('Selling');
+    console.log(req.body);
+    var newSelling = new Selling();
+    newSelling.good = req.body.id;
+    newSelling.phone = req.body.phone;
+    newSelling.source = req.body.source;
+    newSelling.date = Date.now();
 
-	newSelling.save(function(err, selling) {
-		if(err) next(err);
+    newSelling.save(function(err, selling) {
+        if (err) next(err);
 
-		res.status(200).send(selling);
-	});
+        res.status(200).send(selling);
+    });
+}
+
+exports.getAll = function(req, res, next) {
+    var db = req.app.db;
+    var Selling = db.model('Selling');
+    Selling.getAll(function(err, sales) {
+        if (err) return next(err);
+        res.status(200).send(sales);
+    });
+}
+
+exports.deleteSelling = function(req, res, next) {
+    var db = req.app.db;
+    var Selling = db.model('Selling');
+    Selling.findOneAndRemove({_id: req.params.id}, null, function(err, selling) {
+        if(err) next(err);
+        res.status(200).send(selling);
+    });
 }
