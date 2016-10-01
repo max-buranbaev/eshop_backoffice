968c7821fb981add2f9d0051a35eb1056b7ec2fa
max_buranbaev <max@izst.ru>
2016-10-01 20:09:19 +0200

    implement routing

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Dashboard from './components/dashboard.jsx'
-import { Provider } from 'react-redux'
+import Layout from './components/Layout'
+import Catalog from './components/catalog/catalog.jsx'
+import SalesTable from './components/sales/salesTable.jsx'
 import store from './store.js'
 import axios from 'axios'
 
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute, browserHistory, hashHistory } from 'react-router'
 
-
+const router = (
+    <Provider store={ store }>
+        <Router history={ hashHistory }>
+            <Route path="/" component={ Layout }>
+                <IndexRoute component={ Catalog }></IndexRoute>
+                <Route path="/sales" component={ SalesTable }></Route>
+            </Route>
+        </Router>
+    </Provider>
+)
 // Render
 var render = function() {
-  ReactDOM.render(
-    <Provider store={store}>
-      <Dashboard />
-    </Provider>,
-    document.getElementById("dashboard")
-  )
+  ReactDOM.render( router, document.getElementById("root"))
 }
 render();
 
+window.store = store;
 store.subscribe(render);
