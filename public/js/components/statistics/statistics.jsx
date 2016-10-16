4a05003552e36bde77037acd3e01603d25fd66e3
max_buranbaev <max@izst.ru>
2016-10-16 19:21:08 +0200

    adding expenditures to stat

--- public/js/components/statistics/statistics.jsx
+++ public/js/components/statistics/statistics.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchSales } from '../../actions/sales'
+import { read } from '../../actions/expenditures'
 import store from '../../store'
 import moment from 'moment'
 
 class Statistics extends Component {
     componentWillMount() {
         store.dispatch(fetchSales());
+        store.dispatch(read());
     }
 
     render() {
@@ -14,12 +16,13 @@ class Statistics extends Component {
         console.log(sales );
         const rows = [];
         for(let month in sales) {
+            let netProfit = sales[month].profit - sales[month].expenditures;
             rows.push(
                 <tr key={ month }>
-                    <td>{ month }</td>
-                    <td>{ sales[month].cashflow }</td>
-                    <td>{ sales[month].profit }</td>
-                    <td></td>
+                    <td><b>{ sales[month].monthTranslated }</b></td>
+                    <td>{ sales[month].cashflow } ₽</td>
+                    <td>{ sales[month].profit } ₽</td>
+                    <td>{ netProfit }</td>
                 </tr>
             )
 
@@ -45,26 +48,38 @@ class Statistics extends Component {
 }
 
 const getProps = (store) => {
-    const months = {};
+    var expenditureMonths = {};
+    store.expenditures.list.map((expenditure) => {
+        let { date, amount } = expenditure;
+        let key = moment(date).format("MMMM");
+        if(!expenditureMonths[key]) {
+            expenditureMonths[key] = amount;
+        } else {
+            expenditureMonths[key] += amount;
+        }
+    });
+
+    var months = {};
     store.sales.sales.map( (sale) => {
-        let month = moment(sale.date).month();
-        let year = moment(sale.date).year();
-        let key = (month + 1) + '/' + year;
+        let key = moment(sale.date).format("MMMM");
         if(!months[key]) {
             months[key] = {
+                monthTranslated: moment(sale.date).locale('ru').format("MMMM"),
                 cashflow: sale.good.price,
-                profit: (sale.good.price - sale.good.purchasePrice)
+                profit: (sale.good.price - sale.good.purchasePrice),
+                expenditures: expenditureMonths[key]
             };
         } else {
             months[key].cashflow += sale.good.price;
             months[key].profit += (sale.good.price - sale.good.purchasePrice);
         }
-    })
+    });
+
 
     return {
         sales: months
     }
 
-}
+};
 
 export default connect(getProps)(Statistics)
