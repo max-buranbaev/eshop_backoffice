5449ccd8c502a47a6e028484c3e35187d49ec54e
max_buranbaev <max@izst.ru>
2016-10-16 19:17:23 +0200

    global refactoring
    - using async

--- helpers/sync.js
+++ helpers/sync.js
@@ -1,37 +1,44 @@
-"use strict"
+"use strict";
 
 var request = require("request");
 var fs = require("fs");
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
-var getAll = require('../views/goods/index.js').getAll
+var async = require('async');
+var getAll = require('../views/goods/index.js').getAll;
+
 
 exports.sync = function(req, res, next) {
     var db = req.app.db;
     var Category = db.model('Category');
     var Good = db.model('Good');
 
-    request('https://dokis.ru/export.xml', function(error, response, body) {
-        parser.parseString(body, function(err, result) {
-
-            result.shop.categories[0].category.map((cat) => {
-                var callback = function(err, category) {
-                    if (err) next(err);
-                }
-
+    async.waterfall([
+        (callback) => {
+            request('https://dokis.ru/export.xml', (err, response, body) => {
+                callback(err, body);
+            });
+        },
+        (body, callback) => {
+            parser.parseString(body, (err, DOM) => {
+                callback(err, DOM);
+            });
+        },
+        (DOM, callback) => {
+            DOM.shop.categories[0].category.map((cat) => {
                 Category.checkAndAdd({
                         id: cat.$.id,
                         name: cat._
                     },
-                    callback
+                    (err) => {
+                        if(err) callback(err);
+                    }
                 )
             });
-
-            result.shop.offers[0].offer.map((offer) => {
-                var callback = function(err, offer) {
-                    if (err) next(err);
-                }
-
+            callback(null, DOM);
+        },
+        (DOM, callback) => {
+            DOM.shop.offers[0].offer.map((offer) => {
                 Good.checkAndAdd({
                         id: offer.$.id,
                         name: offer.name,
@@ -39,12 +46,19 @@ exports.sync = function(req, res, next) {
                         price: offer.price[0],
                         categorySite: Category.getIdbySiteId(offer.id)
                     },
-                    function() {
-                        return getAll(req, res, next);
+                    (err) => {
+                        if(err) callback(err);
                     }
                 )
-
             });
-        });
-    });
-}
+            callback(null)
+        }
+    ],
+        (err) => {
+            if(err) console.log(err);
+            console.log("Import Done");
+            getAll(req, res, next);
+        }
+
+    )
+};
