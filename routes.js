034b9b695356639413e6bed8bc45dcc1a2e2ae58
Max Buranbaev <Max@Maxs-MacBook-Pro.local>
2016-12-15 14:08:59 +0100

    changing api routes

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var fs = require('fs');
-var config = require('./config');
+const fs = require('fs');
+const config = require('./config');
 
 exports = module.exports = function (app) {
 
@@ -12,22 +12,24 @@ exports = module.exports = function (app) {
         });
     });
 
-    app.get('/goods/', require('./views/goods/index.js').getAll);
-    app.get('/good/:id', require('./views/goods/index.js').getDataById);
+    app.get('/api/goods/', require('./views/goods/index.js').getAll);
+    app.get('/api/good/:id', require('./views/goods/index.js').getDataById);
 
-    app.post('/goods/add', require('./views/goods/index.js').add);
-    app.post('/goods/change', require('./views/goods/index.js').updateById);
-    app.post('/goods/remove', require('./views/goods/index.js').deleteById);
+    app.post('/api/goods/add', require('./views/goods/index.js').add);
+    app.post('/api/goods/change', require('./views/goods/index.js').updateById);
+    app.post('/api/goods/remove', require('./views/goods/index.js').deleteById);
 
 
-    app.get('/sales/', require('./views/sellings/index.js').getAll);
-    app.put('/sales/', require('./views/sellings/index.js').addSelling);
-    app.delete('/sales/:id', require('./views/sellings/index.js').deleteSelling);
+    app.get('/api/sales/', require('./views/sellings/index.js').getAll);
+    app.put('/api/sales/', require('./views/sellings/index.js').addSelling);
+    app.delete('/api/sales/:id', require('./views/sellings/index.js').deleteSelling);
 
-    app.post('/expenditures', require('./views/expenditures/index.js').create);
-    app.get('/expenditures', require('./views/expenditures/index.js').read);
-    app.delete('/expenditures/:id', require('./views/expenditures/index.js').delete);
+    app.post('/api/expenditures', require('./views/expenditures/index.js').create);
+    app.get('/api/expenditures', require('./views/expenditures/index.js').read);
+    app.delete('/api/expenditures/:id', require('./views/expenditures/index.js').delete);
 
-    app.get('/goods/sync/', require('./helpers/sync.js').sync);
+    app.post('/api/stat/', require('./views/stat/index.js').getAll);
+
+    app.get('/api/goods/sync/', require('./helpers/sync.js').sync);
 
 };
