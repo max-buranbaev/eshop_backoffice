28ce14c1b6be0c2eac60197601232df9cbbfc11c
Max Buranbaev <Max@Maxs-MacBook-Pro.local>
2016-12-15 14:08:44 +0100

    method for getting sales by date

--- schemas/selling.js
+++ schemas/selling.js
@@ -1,32 +1,42 @@
-'use strict';
+exports = module.exports = function (app, mongoose) {
 
-exports = module.exports = function(app, mongoose) {
+    const schema = mongoose.Schema({
+        good: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Good',
+            required: true
+        },
+        date: {
+            type: Date,
+            required: true
+        },
+        phone: {
+            type: String
+        },
+        source: {
+            type: String
+        }
+    });
 
-	var schema = mongoose.Schema({
-		good: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Good',
-			reqired: true
-		},
-		date: {
-			type: Date,
-			required: true
-		},
-		phone: {
-			type: String
-		},
-		source: {
-			type: String
-		}
-	});
+    schema.statics.getAll = function(callback) {
+		const Selling = this;
+        Selling.find().populate('good', 'name purchasePrice price').exec(function (err, sales) {
+            if (err) return callback(err);
+            return callback(null, sales);
+        });
+    };
 
-	schema.statics.getAll = function(callback) {
-	  var Selling = this;
-	  Selling.find().populate('good', 'name purchasePrice price').exec(function(err, sales) {
-		if (err) return callback(err);
-		return callback(null, sales);
-	  });
-	};
+    schema.statics.getByPeriod = function(startDate, endDate, callback) {
+        const Selling = this;
+        let query = {
+            date: {
+                $gte: startDate,
+                $lte: endDate
+            }
+        };
 
-	app.db.model('Selling', schema);
+        Selling.find(query).populate('good', 'name purchasePrice price').exec((err, sales) => callback(err, sales));
+    };
+
+    app.db.model('Selling', schema);
 };
