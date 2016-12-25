f848d15adb6717502ecbbec7941fde42c922e3c9
Max Buranbaev <Max@ip-192-168-1-38.is74.loc>
2016-12-25 11:42:32 +0500

    bugfix

--- views/stat/StatGenerator.js
+++ views/stat/StatGenerator.js
@@ -1,8 +1,10 @@
+const moment =require('moment');
+
 module.exports = class StatGenerator {
 
-    constructor(data) {
+    constructor(data, metrik) {
         this.data = data;
-
+        this.metrik = metrik;
         let cashFlow = null;
         let sumOfMarginPercents = null;
         let goodsCost = null;
@@ -18,6 +20,83 @@ module.exports = class StatGenerator {
         this.averageMarginPercent = Math.round((sumOfMarginPercents / this.data.length) * 100) / 100;
         this.profit = cashFlow - goodsCost;
         this.cashFlow = cashFlow;
+
+        // binders
+        this.getProfit = this.getProfit.bind(this);
+        this.getGoodsExpenditures = this.getGoodsExpenditures.bind(this);
+        this.getSumOfSales = this.getSumOfSales.bind(this);
+        this.getCashFlow = this.getCashFlow.bind(this);
+        this.getAverageMarginPercent = this.getAverageMarginPercent.bind(this);
+        this.getProfit = this.getProfit.bind(this);
+        this.getAverageCheck = this.getAverageCheck.bind(this);
     }
 
+    getWeekly() {
+        let result = [];
+        return true;
+    }
+
+    getSumOfSales(dateStart, dateEnd) {
+        let sum = 0;
+        this.data.map(selling => {
+            if(moment(selling.date).format('x') >= moment(dateStart).format('x') && moment(selling.date).format('x') <= moment(dateEnd).format('x')) {
+                sum++;
+            }
+        });
+        return sum;
+    }
+
+    getCashFlow(dateStart, dateEnd, formatted) {
+        let cashFlow = 0;
+        this.data.map(selling => {
+            if(moment(selling.date).format('x') >= moment(dateStart).format('x') && moment(selling.date).format('x') <= moment(dateEnd).format('x')) {
+                cashFlow += selling.good.price;
+            }
+        });
+        console.log(formatted ? cashFlow + "₽" : cashFlow);
+        console.log(this.data);
+        return formatted ? cashFlow + "₽" : cashFlow;
+    }
+
+    getAverageMarginPercent(dateStart, dateEnd, formatted) {
+        let sumOfMarginPercents = 0;
+        let counter = 0;
+        let averageMarginPercent = 0;
+
+        this.data.map(selling => {
+            if(moment(selling.date).format('x') >= moment(dateStart).format('x') && moment(selling.date).format('x') <= moment(dateEnd).format('x')) {
+                sumOfMarginPercents += 100 * (selling.good.price / selling.good.purchasePrice);
+                counter++;
+            }
+        });
+
+        averageMarginPercent = Math.round((sumOfMarginPercents / this.data.length));
+        return formatted ? averageMarginPercent + "%" : averageMarginPercent;
+    }
+
+    getConversion(dateStart, dateEnd, formatted) {
+        console.log(this.metrik);
+    }
+
+    getGoodsExpenditures(dateStart, dateEnd, formatted) {
+        let goodsExpenditures = 0;
+        this.data.map(selling => {
+            if(moment(selling.date).format('x') >= moment(dateStart).format('x') && moment(selling.date).format('x') <= moment(dateEnd).format('x')) {
+                goodsExpenditures += selling.good.purchasePrice;
+            }
+        });
+
+        return formatted ? goodsExpenditures + "₽" : goodsExpenditures;
+    }
+
+    getProfit(dateStart, dateEnd, formatted) {
+        let result = this.getCashFlow(dateStart, dateEnd) / this.getGoodsExpenditures(dateStart, dateEnd);
+        return formatted ? result + "₽" : result;
+    }
+
+    getAverageCheck(dateStart, dateEnd, formatted) {
+        console.log(this.getCashFlow(dateStart, dateEnd));
+        let averageCheck = Math.round((this.getCashFlow(dateStart, dateEnd) / this.getSumOfSales(dateStart, dateEnd)) * 100) / 100;
+        return formatted ? averageCheck + "₽" : averageCheck;
+    }
 };
--- views/stat/index.js
+++ views/stat/index.js
@@ -3,15 +3,14 @@ const axios = require('axios');
 const StatGenerator = require('./StatGenerator');
 
 exports.getAll = (req, res, next) => {
-    const Selling = req.app.db.models.Selling;
     console.log(req.body);
+    const Selling = req.app.db.models.Selling;
     const startDate = moment(req.body.item.startDate, "DD.MM.YYYY");
     const endDate = moment(req.body.item.endDate, "DD.MM.YYYY");
     const yaMetrikApi = `https://api-metrika.yandex.ru/stat/v1/data.json?id=32372795&date1=${startDate.format("YYYY-MM-DD")}&date2=${endDate.format("YYYY-MM-DD")}&metrics=ym:s:users&dimensions=ym:s:<attribution>TrafficSource&oauth_token=AQAAAAAT0xGsAAPpjNcTKg7nkUIJi7UFsCNRKcw`;
 
     const callback = (err, data) => {
         if(err) next(err);
-
         const Stat = new StatGenerator(data);
         axios.get(yaMetrikApi).then(response => {
 
@@ -29,12 +28,12 @@ exports.getAll = (req, res, next) => {
                     endDate: endDate
                 },
                 stat: {
-                    averageCheck: Stat.averageCheck,
-                    averageMarginPercent: Stat.averageMarginPercent,
-                    conversion: Math.round((data.length / sumOfVisitors) * 10000) / 100,
-                    profit: Stat.profit,
-                    cashflow: Stat.cashFlow,
-                    sumOfSales: Stat.sumOfSales
+                    averageCheck: Stat.getAverageCheck(startDate.format(), endDate.format(), true),
+                    averageMarginPercent: Stat.getAverageMarginPercent(startDate.format(), endDate.format(), true),
+                    conversion: Stat.getConversion(startDate.format(), endDate.format(), true),
+                    profit: Stat.getProfit(startDate.format(), endDate.format(), true),
+                    cashflow: Stat.getCashFlow(startDate.format(), endDate.format(), true),
+                    sumOfSales: Stat.getSumOfSales(startDate.format(), endDate.format())
                 }
             };
 
@@ -46,3 +45,39 @@ exports.getAll = (req, res, next) => {
     Selling.getByPeriod(startDate.format(), endDate.format(), callback);
 };
 
+exports.getWeekly = (req, res, next) => {
+    const Selling = req.app.db.models.Selling;
+    const startDate = moment("01.09.2016", "DD.MM.YYYY");
+    const endDate = moment(req.body.item.endDate, "DD.MM.YYYY");
+    const yaMetrikApi = `https://api-metrika.yandex.ru/stat/v1/data.json?id=32372795&date1=${startDate.format("YYYY-MM-DD")}&date2=${endDate.format("YYYY-MM-DD")}&metrics=ym:s:users&dimensions=ym:s:<attribution>TrafficSource&oauth_token=AQAAAAAT0xGsAAPpjNcTKg7nkUIJi7UFsCNRKcw`;
+
+    const callback = (err, data) => {
+        if(err) next(err);
+
+
+        axios.get(yaMetrikApi).then(response => {
+
+            const Stat = new StatGenerator(data, response.data.data);
+
+            let result = {
+                query: {
+                    startDate: startDate,
+                    endDate: endDate
+                },
+                stat: {
+                    averageCheck: Stat.getAverageCheck(startDate.format(), endDate.format(), true),
+                    averageMarginPercent: Stat.getAverageMarginPercent(startDate.format(), endDate.format(), true),
+                    conversion: Stat.getConversion(startDate.format(), endDate.format(), true),
+                    profit: Stat.getProfit(startDate.format(), endDate.format(), true),
+                    cashflow: Stat.getCashFlow(startDate.format(), endDate.format(), true),
+                    sumOfSales: Stat.getSumOfSales(startDate.format(), endDate.format())
+                }
+            };
+
+            res.send(result);
+        });
+
+    };
+
+    Selling.getByPeriod(startDate.format(), endDate.format(), callback);
+};
